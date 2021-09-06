1e8f46f03e63914b14d55ce648fee773d0214e25
Matthew L Davis <mld.0@protonmail.com>
2021-09-06 21:25:50 +1000

    add 'get_words()', 'remove()'
    We use an empty TrieNode() instead of None (from ans) to replace nodes when
    deleting, since having None nodes breaks 'get_words()' - appears to function
    the same, is correct as per 'search()'?

--- trie.py
+++ trie.py
@@ -6,11 +6,15 @@ class TrieNode(object):
         self.children = defaultdict(None)
         self.isEndOfWord = False
 
-
 class Trie(object):
 
     def __init__(self):
         self.root = TrieNode()
+
+
+    def node_is_empty(self, node):
+        return len(node.children.keys()) == 0
+
 
     def insert(self, key):
         """Insert word in Trie"""
@@ -26,7 +30,7 @@ class Trie(object):
 
         loop_node.isEndOfWord = True
 
-    
+
     def search(self, key):
         """Search for word in Trie"""
         loop_node = self.root
@@ -42,22 +46,52 @@ class Trie(object):
         return loop_node.isEndOfWord
 
 
-    def display(self, node, text, level):
-        """Print words in Trie"""
-        if node.isEndOfWord != False:
-            print("\t%s" % text)
+    def get_words(self, node=None, text="", level=0):
+        """Get list of words in Trie"""
+        result = []
+        if node is None:
+            node = self.root
+        if node.isEndOfWord:
+            result.append(text)
 
-        for i in sorted(node.children.keys()):
-            text_list = list(text)
-            text_list[level] = i
-            text = ''.join(text_list)
-            self.display(node.children[i], text, level+1)
-        
+        for c in sorted(node.children.keys()):
+            text_list = list(text + " ")
+            text_list[level] = c
+            text = ''.join(text_list).strip()
+            for word in self.get_words(node.children[c], text, level+1):
+                result.append(word)
+
+        return result
+
+
+    def remove(self, key, node=None, depth=0):
+        if node is None:
+            node = self.root
+
+        #   if processing last character of key
+        if (depth == len(key)):
+
+            if node.isEndOfWord:
+                node.isEndOfWord = False
+
+            #   if node is not a prefix of another word, remove it
+            if self.node_is_empty(node):
+                node = TrieNode()
+
+            return node
+
+        #   recurse for child corresponding to next character of key
+        index = key[depth]
+        node.children[index] = self.remove(key, node.children[index], depth+1)
+
+        #   if node has no children and is not end of another word, remove it
+        if self.node_is_empty(node) and node.isEndOfWord == False:
+            node = TrieNode()
+
+        return node
+
 
     #   TODO: 2021-09-06T19:05:29AEST _algorithms, trie, implement remove(), print(), longest_matching_prefix()
-    def remove(self, key, depth=0):
-        raise Exception("Unimplemented")
-
     def longest_matching_prefix(self, input_str):
         raise Exception("Unimplemented")
 
@@ -72,20 +106,25 @@ def main():
         t.insert(key)
 
     search_list = [ "the", "these", "their", "thaw" ]
-
     for loop_search in search_list:
         result = t.search(loop_search)
         print("loop_search=(%s), result=(%s)" % (loop_search, result))
 
-    text = " " * 20
-    level = 0
-    print("t.display:")
-    t.display(t.root, text, level)
+    words_list = t.get_words()
+    print("words_list=(%s)" % words_list)
 
+    t.remove("answer")
+    t.remove("their")
+
+    search_list = [ "the", "these", "their", "thaw" ]
+    for loop_search in search_list:
+        result = t.search(loop_search)
+        print("loop_search=(%s), result=(%s)" % (loop_search, result))
+
+    words_list = t.get_words()
+    print("words_list=(%s)" % words_list)
 
 if __name__ == "__main__":
     main()
-
-
 
 
