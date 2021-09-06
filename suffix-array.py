428a2e400b2291bf04e7ebf5242eb84e375a67b2
Matthew L Davis <mld.0@protonmail.com>
2021-09-06 19:09:11 +1000

    ongoing

--- suffix-array.py
+++ suffix-array.py
@@ -7,7 +7,7 @@ from random import randint
 #   {{{2
 
 
-def find_suffix_array(s: str) -> list[int]:
+def get_suffix_array(s: str) -> list[int]:
     result = [ x[1] for x in sorted( [s[i:],i] for i in range(len(s)) ) ]
     return result
 
@@ -27,17 +27,23 @@ def binary_search_suffix_array(pattern: str, text: str, suffix_array: list[int])
             l = mid + 1
     return None
 
+
 #   TODO: 2021-09-03T03:12:28AEST _algorithms, suffix-arrays, (applications of): longest_repeated_substring, longest_common_substring, longest_palindromic_substring
-def longest_repeated_substring(s: str) -> str:
-    pass
-def longest_common_substring(s1: str, s2: str) -> str:
-    pass
+#   Ongoing: 2021-09-06T17:50:05AEST _algorithms, applications of suffix-array vs suffix-tree (which is better suited) for longest_repeated_substring, longest_common_substring, longest_palindromic_substring -> implementations for both?
+
 def longest_palindromic_substring(s: str) -> str:
     pass
 
+def longest_repeated_substring(s: str) -> str:
+    pass
+
+def longest_common_substring(s1: str, s2: str) -> str:
+    pass
+
+
 text = "banana"
 search = "nan"
-suffix_array = find_suffix_array(text)
+suffix_array = get_suffix_array(text)
 result = binary_search_suffix_array(search, text, suffix_array)
 print("text=(%s)\nsuffix_array=(%s)" % (text, str(suffix_array)))
 print("search=(%s), result=(%s)" % (search, str(result)))
@@ -47,7 +53,7 @@ length = 50
 get_random_str = lambda x: ''.join(chr(randint(ord('a'), ord('z'))) for x in range(int(x)))
 text = get_random_str(length)
 search = text[17:23]
-suffix_array = find_suffix_array(text)
+suffix_array = get_suffix_array(text)
 result = binary_search_suffix_array(search, text, suffix_array)
 print("text=(%s)\nsuffix_array=(%s)" % (text, str(suffix_array)))
 print("search=(%s), result=(%s)" % (search, str(result)))
@@ -89,3 +95,4 @@ print()
 #result = suffix_array_naive(s)
 #print("suffix_array_naive: '%s'" % s)
 #print("result=(%s)" % str(result))
+
