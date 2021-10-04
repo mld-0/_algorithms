f33e81b56f6df5d3ea99c8e06fa3ff67b06a36ed
Matthew L Davis <mld.0@protonmail.com>
2021-10-04 12:43:35 +1100

    add min-in-rotated-sorted-list
    As taken from leetcode.

--- binary-search.py
+++ binary-search.py
@@ -11,7 +11,7 @@ def binary_search(nums: list[int], target: int) -> int:
 
     while l <= r:
         mid = (r + l) // 2
-        #   alternatively, less likely to overflow: 
+        #   alternatively: 
         #   mid = l + (r - l) // 2
         if nums[mid] == target:
             return mid
@@ -29,6 +29,8 @@ def bsearch_find_left(nums: List[int], target: int) -> List[int]:
     r = len(nums) - 1
     while l <= r:
         mid = (r + l) // 2
+        #   alternatively: 
+        #   mid = l + (r - l) // 2
         if nums[mid] == target:
             while mid-1 >= 0 and nums[mid-1] == target:
                 mid = mid - 1
@@ -46,6 +48,8 @@ def bsearch_find_right(nums: List[int], target: int) -> List[int]:
     r = len(nums) - 1
     while l <= r:
         mid = (r + l) // 2
+        #   alternatively: 
+        #   mid = l + (r - l) // 2
         if nums[mid] == target:
             while mid+1 < len(nums) and nums[mid+1] == target:
                 mid = mid + 1
@@ -57,6 +61,28 @@ def bsearch_find_right(nums: List[int], target: int) -> List[int]:
     return -1
 
 
+def bsearch_findMin(nums: List[int]) -> int:
+    """Binary search for index about which the list has been rotated (index of min element in rotated sorted list)"""
+    l = 0
+    r = len(nums) - 1
+    if len(nums) == 1:
+        return nums[0]
+    if nums[l] < nums[r]:
+        return nums[l]
+    while l <= r:
+        mid = l + (r - l) // 2
+        #   alternatively: 
+        #   mid = l + (r - l) // 2
+        if nums[mid] > nums[mid+1]:
+            return nums[mid+1]
+        if nums[mid-1] > nums[mid]:
+            return nums[mid]
+        if nums[l] < nums[mid]:
+            l = mid + 1
+        else:
+            r = mid - 1
+
+
 def test_binary_search():
     #   {{{
     input_values = [ ( [-1,0,3,5,9,12], 9 ), ( [-1,0,3,5,9,12], 2 ), ( [5], 5 ) ]
@@ -64,7 +90,7 @@ def test_binary_search():
     for (nums, target), check in zip(input_values, input_checks):
         result = binary_search(nums, target)
         print("result=(%s)" % str(result))
-        assert( result == check )
+        assert result == check, "Check failed"
     #   }}}
 
 def test_bsearch_find_ends():
@@ -79,6 +105,17 @@ def test_bsearch_find_ends():
         assert result == check, "Check failed"
     #   }}}
 
+def test_bsearch_findMin():
+    #   {{{
+    input_values = [ [3,4,5,1,2], [4,5,6,7,0,1,2], [11,13,15,17], [1,2], [2,3,4,5,6,1], [284,287,289,293,295,298,0,3,8,9,10,11,12,15,17,19,20,22,26,29,30,31,35,36,37,38,42,43,45,50,51,54,56,58,59,60,62,63,68,70,73,74,81,83,84,87,92,95,99,101,102,105,108,109,112,114,115,116,122,125,126,127,129,132,134,136,137,138,139,147,149,152,153,154,155,159,160,161,163,164,165,166,168,169,171,172,174,176,177,180,187,188,190,191,192,198,200,203,204,206,207,209,210,212,214,216,221,224,227,228,229,230,233,235,237,241,242,243,244,246,248,252,253,255,257,259,260,261,262,265,266,268,269,270,271,272,273,277,279,281], [1], ]
+    input_checks = [ min(x) for x in input_values ]
+    for nums, check in zip(input_values, input_checks):
+        print("nums=(%s)" % nums)
+        result = bsearch_findMin(nums)
+        print("result=(%s)" % result)
+        assert result == check, "Check failed"
+    #   }}}
+
 
 if __name__ == '__main__': 
     print("test_binary_search():")
@@ -86,5 +123,8 @@ if __name__ == '__main__':
     print()
     print("test_bsearch_find_ends():")
     test_bsearch_find_ends()
+    print()
+    print("test_bsearch_findMin():")
+    test_bsearch_findMin()
     print()
 
