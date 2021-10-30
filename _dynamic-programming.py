ed74e5ae17d208a9f613ccdff51b3d2d2835e840
Matthew L Davis <mld.0@protonmail.com>
2021-10-30 14:45:36 +1100

    add example coinChange
    As taken from leetcode. Min number of coins that sum to a given value.
    Top-down and bottom-up solutions.

--- _dynamic-programming.py
+++ _dynamic-programming.py
@@ -2,8 +2,8 @@
 #   vim: set tabstop=4 modeline modelines=10 foldmethod=marker:
 #   vim: set foldlevel=2 foldcolumn=2:
 #   }}}1
-import math
 from typing import List
+import math
 #   {{{2
 
 #   TODO: 2021-09-30T20:22:47AEST _algorithms, dynamic-programming overview/compilation
@@ -31,18 +31,6 @@ def pascals_triangle(numRows: int) -> List[List[int]]:
             result[row][col] = result[row-1][col-1] + result[row-1][col]
 
     return result
-
-
-def test_pascals_triangle():
-    #   {{{
-    input_values = [ 5, 1, ]
-    input_checks = [ [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]], [[1]], ]
-    for numRows, check in zip(input_values, input_checks):
-        print("numRows=(%s)" % numRows)
-        result = pascals_triangle(numRows)
-        print("result=(%s)" % str(result))
-        assert result == check, "Check failed"
-    #   }}}
 
 
 def minFallingPathSum_TopDown(matrix: List[List[int]]) -> int:
@@ -94,6 +82,66 @@ def minFallingPathSum_BottomUp(matrix: List[List[int]]) -> int:
     return min(grid[-1])
 
 
+def coinChange_DP_TopDown(coins: List[int], amount: int) -> int:
+    """Determine minimum number of values from 'coins' (with replacement) that sum to 'amount'"""
+    memo = dict()
+
+    def solve(remaining):
+        if remaining in memo:
+            return memo[remaining]
+
+        if remaining < 0:
+            memo[remaining] = -1
+            return -1
+        if remaining == 0:
+            memo[remaining] = 0
+            return 0
+
+        minimum = math.inf
+        for c in coins:
+            result = solve(remaining-c)
+            if result >= 0 and result < minimum:
+                minimum = 1 + result
+
+        if minimum == math.inf:
+            memo[remaining] = -1
+            return -1
+        else:
+            memo[remaining] = minimum
+            return minimum
+
+    return solve(amount)
+
+
+#   runtime: beats 86%
+def coinChange_DP_BottomUp(coins: List[int], amount: int) -> int:
+    """Determine minimum number of values from 'coins' (with replacement) that sum to 'amount'"""
+    #   table[i]: minimum number of coins that sum to amount
+    table = [ math.inf for x in range(amount+1) ]
+    table[0] = 0
+
+    for c in coins:
+        for x in range(c, amount+1):
+            table[x] = min(table[x], table[x-c] + 1)
+
+    if table[amount] == math.inf:
+        return -1
+    else:
+        return table[amount]
+
+
+
+def test_pascals_triangle():
+    #   {{{
+    input_values = [ 5, 1, ]
+    input_checks = [ [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]], [[1]], ]
+    for numRows, check in zip(input_values, input_checks):
+        print("numRows=(%s)" % numRows)
+        result = pascals_triangle(numRows)
+        print("result=(%s)" % str(result))
+        assert result == check, "Check failed"
+    #   }}}
+
 def test_minFallingPathSum():
     #   {{{
     input_values = [ [[2,1,3],[6,5,4],[7,8,9]], [[-19,57],[-40,-5]], [[-48]], [[100,-42,-46,-41],[31,97,10,-10],[-58,-51,82,89],[51,81,69,-51]], [[17,82],[1,-44]], ]
@@ -107,7 +155,22 @@ def test_minFallingPathSum():
         assert result == check, "Check failed"
     #   }}}
 
-def main():
+def test_coinChange():
+    #   {{{
+    test_functions = [ coinChange_DP_TopDown, coinChange_DP_BottomUp, ]
+    input_values = [ ([1,2,5], 11), ([2], 3), ([1], 0), ([1], 1), ([1], 2), ]
+    input_checks = [ 3, -1, 0, 1, 2, ]
+    for test_func in test_functions:
+        print(test_func.__name__)
+        for (coins, amount), check in zip(input_values, input_checks):
+            print("coins=(%s), amount=(%s)" % (coins, amount))
+            result = test_func(coins, amount)
+            print("result=(%s)" % result)
+            assert result == check, "Check comparison failed"
+        print()
+    #   }}}
+
+if __name__ == '__main__':
     #   {{{
     print("test_pascals_triangle():")
     test_pascals_triangle()
@@ -115,9 +178,6 @@ def main():
     print("test_minFallingPathSum():")
     test_minFallingPathSum()
     print()
+    test_coinChange()
     #   }}}
-
-
-if __name__ == '__main__':
-    main()
 
